c8aed5eab516bce86a27bac6611a821d4a7391c3
Asylbek <asola@list.ru>
2024-01-31 12:06:45 +0600

    updates

---
+++ django_auth/users/views.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.views.generic import CreateView
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+
+# Создаем здесь представления.
+def home(request):
+    return render(request, "users/home.html")
+
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = "registration/signup.html"
+
